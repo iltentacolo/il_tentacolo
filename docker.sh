38f95f03dcf01cee6a851de185ae3dcf1e5d9cee
Francesco Sullo <sullof@gmail.com>
2025-10-21 14:42:58 +0200

    Add docker script

---
+++ docker.sh
@@ -0,0 +1,11 @@
+# Stop and remove container only if it exists
+docker stop iltentacolo-sullo-co 2>/dev/null || true
+docker rm iltentacolo-sullo-co 2>/dev/null || true
+
+docker run --name iltentacolo-sullo-co \
+  -p 8040 \
+  --restart unless-stopped \
+  -e VIRTUAL_HOST=iltentacolo.sullo.co \
+  -e LETSENCRYPT_HOST=iltentacolo.sullo.co \
+  -e LETSENCRYPT_EMAIL=iltentacolo@sullo.co \
+  -v `pwd`/il_tentacolo.com/dist:/usr/share/nginx/html:ro -d nginx
