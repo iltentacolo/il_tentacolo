49c7b342b828f1bd0e15c5344723d070e160441a
Francesco Sullo <sullof@gmail.com>
2025-10-21 14:52:19 +0200

    Trying changing the port

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@ docker stop iltentacolo-sullo-co 2>/dev/null || true
 docker rm iltentacolo-sullo-co 2>/dev/null || true
 
 docker run --name iltentacolo-sullo-co \
-  -p 80:80 \
+  -p 8040 \
   --restart unless-stopped \
   -e VIRTUAL_HOST=iltentacolo.sullo.co \
   -e LETSENCRYPT_HOST=iltentacolo.sullo.co \
