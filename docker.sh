6cb7dc622e9a675366e7d328ddf54365a2cb234b
Francesco Sullo <sullof@gmail.com>
2025-10-23 12:13:30 +0200

    Trying to solve the routing issue

--- docker.sh
+++ docker.sh
@@ -7,4 +7,6 @@ docker run --name iltentacolo-sullo-co \
   -e VIRTUAL_HOST=iltentacolo.sullo.co \
   -e LETSENCRYPT_HOST=iltentacolo.sullo.co \
   -e LETSENCRYPT_EMAIL=iltentacolo@sullo.co \
-  -v $(pwd)/dist:/usr/share/nginx/html:ro -d nginx
+  -v $(pwd)/dist:/usr/share/nginx/html:ro \
+  -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+  -d nginx
